e9410802903839a54420e40d09e64ade9ee20478
andrea lee <lee.ays.lee@gmail.com>
2017-10-30 11:16:38 -0400

    set up user model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const db = require('../db/config');
+
+const User = {};
+
+//findByUserName Method
+User.findByUserName = userName => {
+  return db.oneOrNone(`
+    SELECT * FROM users
+    WHERE username = $1
+    `, [userName]);
+};
+
+//create method
+User.create = user => {
+    return db.one(`
+      INSERT INTO users
+      (username, email, password_digest, firstname, lastname)
+      VALUES ( $1, $2, $3, $4, $5)
+      RETURNING *
+      `, [user.username, user.email, user.password_digest, user.firstname, user.lastname]);
+  };
+
+  module.exports = User;
