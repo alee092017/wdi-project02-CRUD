1dfb849ca985ea8fee951b70ed720e1c4e6dc752
andrea lee <lee.ays.lee@gmail.com>
2017-10-30 10:10:37 -0400

    fixed syntax error in migration file-tables successfully created

--- db/migrations/migration102920170903.sql
+++ db/migrations/migration102920170903.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
-  password_digest TEXT NOT NULL
+  password_digest TEXT NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   firstname VARCHAR(255),
   lastname VARCHAR(255)
