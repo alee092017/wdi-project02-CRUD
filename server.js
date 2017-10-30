3ae3797880d797362e80bb6a0a37fdf126be8615
andrea lee <lee.ays.lee@gmail.com>
2017-10-30 10:58:45 -0400

    added auth packages/middleware to the server

--- server.js
+++ server.js
@@ -1,37 +1,48 @@
+//import dependencies
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = ('method-override');
+//need below for auth
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
+//initialize app
 const app = express();
-// reqire('dotenv').config();
+//add dot env file for auth function
+reqire('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-});
-
-app.set('view enging', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+//middleware--go back and make sure you know line by line
+//what's going on here.
 app.use(logger('dev'));
-// app.use(express.static(path.join(dirname, 'public')));
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+//middleware--auth
 app.use(cookieParser());
 app.use(session({
   secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: true,
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
+
+// static files!!  this is not just index this your css & potentially other assets & things ie src
+app.use(express.static(path.join(dirname, 'public')));
+
+//views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//set the port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+});
+
 
 // app.get('/', (req, res) => {
 //   res.render('index');
